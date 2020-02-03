abdec67b35ca483d9b10980282350ad6fb998b90
Sudheer Hebbale <sudheer.hr@tekenlight.com>
2020-02-03 07:22:16 +0530

    added a test case for async file interface

---
+++ evluaserver/samples/LUA/src/vidserver.lua
@@ -0,0 +1,54 @@
+local handlers = {};
+handlers.handle_request = function () -- {
+	local return_string = 'Hello from LUA handler';
+	local request = platform.get_http_request();
+	local form = request:parse_req_form();
+	local response = platform.get_http_response();
+	response:set_chunked_trfencoding(true);
+	response:set_content_type("video/mp4");
+	response:send();
+
+	local fh, err = platform.file_open("./SatisfiabilityAndCookTheorem.mp4", "r");
+	--local fh, err = platform.file_open("./Sudheer.JPG", "r");
+	--local fh, err = platform.file_open("/etc/krb5.keytab", "r");
+	if (err ~= nil) then error(err); end
+	if (err1 ~= nil) then error(err1); end
+	local i = 0;
+	local j = 0;
+	local buffer = platform.alloc_buffer(4096);
+	local n, msg = fh:read_binary(buffer, 4096);
+	local ret = 0;
+	while (n ~= 0) do -- {
+		i = i + 1;
+		j = j + 1;
+		if (i == 1) then --{
+			--print('hhhhhh');
+			--print(n..' +');
+			response:write(buffer, n);
+		end -- }
+		--print('hihihihi');
+		if (n == -1) then -- {
+			print(msg);
+			break;
+		end -- }
+		n, msg = fh:read_binary(buffer, 4096);
+		--print('chachi');
+		--print(n..' +');
+		if (j == 1000) then -- {
+			print("1000 packets sent");
+			j = 0;
+		end -- }
+		response:write(buffer, n);
+	end -- }
+	fh:close();
+
+
+	return ;
+end -- }
+
+local arg = {...}
+req_handler_func_name = arg[2];
+local func = handlers[req_handler_func_name];
+
+return pcall(func);
+
