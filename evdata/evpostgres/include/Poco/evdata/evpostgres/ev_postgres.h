92ffa17f6179e40a7b1b967ab55681198d8c8257
Sudheer Hebbale <sudheer.hr@tekenlight.com>
2021-10-14 08:41:58 +0530

    Porting related cmake changes

--- evdata/evpostgres/include/Poco/evdata/evpostgres/ev_postgres.h
+++ evdata/evpostgres/include/Poco/evdata/evpostgres/ev_postgres.h
@@ -1,10 +1,14 @@
 extern "C" {
+#ifdef LOCAL_DEVELOPMENT
+#include "libpq-fe.h"
+#include "catalog/pg_type_d.h"
+#else
 #if defined (PG_VERSION) && (PG_VERSION == 12) 
 #include "postgresql/libpq-fe.h"
 #include "postgresql/12/server/catalog/pg_type_d.h"
 #else
-#include "libpq-fe.h"
-#include "catalog/pg_type_d.h"
+#error
+#endif
 #endif
 }
 #include <Poco/evdata/ev_sql_access.h>
