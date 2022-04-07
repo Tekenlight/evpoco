fe9c89e1d947c2942897227f56f0b877cdcf93e2
Atul Krishna <atultherajput@gmail.com>
2022-04-07 08:42:00 +0000

    Added dummy files in .../include/Poco dirs.

--- evdata/evmysql/src/ev_main.cpp
+++ evdata/evmysql/src/ev_main.cpp
@@ -1,15 +1,17 @@
 #include "Poco/evdata/evmysql/ev_mysql.h"
 
+extern "C" {
 int ev_mysql_connection(lua_State *L);
 int ev_mysql_statement(lua_State *L);
+}
 
 /*
  * library entry point
  */
-LUA_EXPORT int luaopen_ev_mysql(lua_State *L) {
+extern "C" int luaopen_evrdbms_mysql(lua_State *L);
+LUA_EXPORT int luaopen_evrdbms_mysql(lua_State *L) {
     ev_mysql_statement(L);
     ev_mysql_connection(L);
 
     return 1;
 }
-
