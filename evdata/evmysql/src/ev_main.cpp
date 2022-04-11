58cd6e645906d92e51f16693f5f44c600bb6afc9
Atul Krishna <atultherajput@gmail.com>
2022-04-11 07:02:54 +0000

    Removed LUA_EXPORT for mysql main.cpp

--- evdata/evmysql/src/ev_main.cpp
+++ evdata/evmysql/src/ev_main.cpp
@@ -1,3 +1,4 @@
+#include "Poco/evnet/evnet.h"
 #include "Poco/evdata/evmysql/ev_mysql.h"
 
 extern "C" {
@@ -9,7 +10,8 @@ int ev_mysql_statement(lua_State *L);
  * library entry point
  */
 extern "C" int luaopen_evrdbms_mysql(lua_State *L);
-LUA_EXPORT int luaopen_evrdbms_mysql(lua_State *L) {
+int luaopen_evrdbms_mysql(lua_State *L)
+{
     ev_mysql_statement(L);
     ev_mysql_connection(L);
 
