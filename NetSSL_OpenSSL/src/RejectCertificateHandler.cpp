e8b0a1bb34633397d649030e31a37d37b2c3bcac
Sudheer Hebbale <sudheer.hr@tekenlight.com>
2023-06-27 04:14:56 +0000

    Changes as part of code review

--- NetSSL_OpenSSL/src/RejectCertificateHandler.cpp
+++ NetSSL_OpenSSL/src/RejectCertificateHandler.cpp
@@ -12,12 +12,30 @@
 //
 
 
+#include "Poco/Util/Application.h"
 #include "Poco/Net/RejectCertificateHandler.h"
 
 
 namespace Poco {
 namespace Net {
 
+static Poco::Util::AbstractConfiguration& appConfig()
+{
+	try
+	{
+		return Poco::Util::Application::instance().config();
+	}
+	catch (Poco::NullPointerException&)
+	{
+		throw Poco::IllegalStateException(
+			"An application configuration is required to initialize the Poco::Net::SSLManager, "
+			"but no Poco::Util::Application instance is available."
+		);
+	}
+}
+
+const std::string RejectCertificateHandler::IGNORE_CA_CERT_ERR_20("ignoreCACertErr20");
+const std::string RejectCertificateHandler::CFG_OPENSSL_CA_CERT_PREFIX("OpenSSL.CACert.");
 
 RejectCertificateHandler::RejectCertificateHandler(bool server): InvalidCertificateHandler(server)
 {
@@ -31,10 +49,15 @@ RejectCertificateHandler::~RejectCertificateHandler()
 
 void RejectCertificateHandler::onInvalidCertificate(const void*, VerificationErrorArgs& errorCert)
 {
-	if (errorCert.errorNumber() == 20)
+	Poco::Util::AbstractConfiguration& config = appConfig();
+	bool ignoreErr20 = config.getBool(CFG_OPENSSL_CA_CERT_PREFIX + IGNORE_CA_CERT_ERR_20, false);
+
+	if (errorCert.errorNumber() == 20 && ignoreErr20) {
 		errorCert.setIgnoreError(true);
-	else
+	}
+	else {
 		errorCert.setIgnoreError(false);
+	}
 }
 
 
