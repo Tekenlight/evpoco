28f184182e2927e7259ecdd793c0aaa1988d8bc3
Ubuntu <sudheer.hr@tekenlight.com>
2026-02-03 15:16:11 +0000

    minor edits

--- evluaserver/samples/LUA/evl_crypto/pgp.lua
+++ evluaserver/samples/LUA/evl_crypto/pgp.lua
@@ -48,7 +48,7 @@ end
 
 print(debug.getinfo(1).source, debug.getinfo(1).currentline, type(symmetric_key));
 
-local ct_s = ffi.new("hex_data_s_type", 0);
+local ct_s = cu.new_hex_data_s_type();
 local ctp = ffi.cast("cipher_text_s*", ct);
 ct_s.value = ctp.buffer;
 ct_s.size = ctp.len;
--- lualib/include/Poco/lualib/lualib.h
+++ lualib/include/Poco/lualib/lualib.h
@@ -1,17 +1,5 @@
 #ifndef LUALIB_H_INCLUDED
 #define LUALIB_H_INCLUDED
 
-typedef struct {
-    size_t size;
-    unsigned char* value;
-} binary_data_s1_type, binary_data_p1_type;
-
-typedef struct {
-    size_t size;
-    unsigned char* value;
-} binary_data_s2_type, binary_data_p2_type;
-
-typedef binary_data_s1_type hex_data_s_type;
-typedef binary_data_s2_type b64_data_s_type;
 
 #endif // LUALIB_H_INCLUDED
